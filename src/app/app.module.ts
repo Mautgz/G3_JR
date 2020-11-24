ba9fd4ef007c87534f3026eee3348c96b74e0f53
Fernando Gomez <topguncounter@hotmail.com>
2020-11-23 21:12:05 -0500

    Esquema frontend- G3_JR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth/auth.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ClientesComponent } from './pages/clientes/clientes.component';
+import { ProductosComponent } from './pages/productos/productos.component';
+import { VentasComponent } from './pages/ventas/ventas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    LoginComponent,
+    RegisterComponent,
+    ClientesComponent,
+    ProductosComponent,
+    VentasComponent
   ],
   imports: [
     BrowserModule
